b4a41ad9de40f030bf2e594f0af7cdbdbca846e7
KumarPatil <kmrpatil323@gmail.com>
2021-06-21 21:11:26 +0530

    Responsive navbar with scroll effect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Navbar from './componantes/Navbar/Navbar';
-
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+import Scrollbars from 'react-custom-scrollbars';
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      
+    <Switch>
+    <Route exact path="/">
+    
+    <Home/>
+    </Route>
+    <Route  path="/contact">
+      <Contact/>
+    </Route>
+    <Route  path="/about">
+      <About/>
+    </Route>
+
+    </Switch>
+
+    
     </div>
   );
 }
